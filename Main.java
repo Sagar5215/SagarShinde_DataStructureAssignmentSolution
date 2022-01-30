dc28949ab338f752fd7941a27f24866718e08348
Sagar5215 <95182125+Sagar5215@users.noreply.github.com>
2022-01-30 20:28:56 +0530

    Data Structure Assignment - Skyscraper building
    Data Structure Graded Project

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+    public static int totalFloors;
+    public static int[] totalFloorSize;
+
+    public static void main(String[] args) {
+        Driver myDriverObj = new Driver();
+        myDriverObj.getInputs();
+        myDriverObj.calculateAndPrintOrderOfConstruction(myDriverObj.totalFloorSize);
+    }
+}
